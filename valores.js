16cf854121bde8a5989bf2e020e5d6f142b0784e
navinocrala <navinocrala@gmail.com>
2022-09-09 20:46:25 -0400

    Primer commit

---
+++ valores.js
@@ -0,0 +1,17 @@
+40
+
+"Diego de Granda"
+
+true
+false
+
+null
+undefined
+
+"array o arreglo"
+[1,2,3]
+
+"Objeto"
+{Nombre: "Diego"}
+
+ivan= 2+2;
---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Iván";
+
+// declarar
+var edad;
+
+edad
+// inicializar 
+edad = 30;
+
+edad
+
+var elementos = ["computadora", "Celular"];
+
+var persona = {
+    nombre: "Iván",
+    edad: 30
+}
+
+persona
